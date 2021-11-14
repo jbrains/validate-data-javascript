09ccf17f71cf9d467ac24275980b599e67756db0
J. B. Rainsberger <me@jbrains.ca>
2021-11-14 11:23:13 -0400

    [FAILING] Intentionally wrote failing tests, so that we could see how zora differs from xUnit.

--- test/hookup_test.js
+++ test/hookup_test.js
@@ -1,1 +1,10 @@
 import { test } from "zora";
+
+test(`Is this thing on?`, (assertions) => {
+  assertions.ok(false, `This fails intentionally.`);
+  assertions.equal(
+    true,
+    false,
+    `true should be false, which is the opposite order of the xUnit tradition.`
+  );
+});
