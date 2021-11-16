36115cce17557d84bde1198163103ffc4b317f01
chbndrhnns <chbndrhnns@users.noreply.github.com>
2021-11-16 13:21:06 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- test/hookup_test.js
+++ test/hookup_test.js
@@ -1,14 +1,29 @@
 import { test } from "zora";
 
 function isPositive(num){
-  return false;
+  return num > 0;
+}
+
+function isNegative(num) {
+  return !isPositive(num);
 }
 
 test(`positive?`, (assertions) => {
-  
+
   assertions.equal(
     isPositive(37),
-    true,
-    `37 is a positive number and our validator should figure that out.`
+    true
+  );
+  assertions.equal(
+    isPositive(-1),
+    false
+  );
+});
+
+test(`negative?`, (assertions) => {
+
+  assertions.equal(
+    isNegative(-1),
+    true
   );
 });
