0b503a84d7904c8434e8e5d2f8d1758a69f55c1e
J. B. Rainsberger <me@jbrains.ca>
2021-11-30 08:59:21 -0400

    Partial application for evenAndPositive

--- test/validate_integers_test.js
+++ test/validate_integers_test.js
@@ -94,14 +94,19 @@ test(`is even and negative?`, (assertions) => {
 });
 
 test(`is even and positive?`, (assertions) => {
-  assertions.equal(validateAnd(2, validateEven, validatePositive), {
+  const evenAndPositiveValidation = combineValidationsWithAnd(
+    validateEven,
+    validatePositive
+  );
+
+  assertions.equal(evenAndPositiveValidation(2), {
     result: true,
   });
-  assertions.equal(validateAnd(-2, validateEven, validatePositive), {
+  assertions.equal(evenAndPositiveValidation(-2), {
     result: false,
     errors: ["-2 is not positive"],
   });
-  assertions.equal(validateAnd(-1, validateEven, validatePositive), {
+  assertions.equal(evenAndPositiveValidation(-1), {
     result: false,
     errors: ["-1 is not even", "-1 is not positive"],
   });
