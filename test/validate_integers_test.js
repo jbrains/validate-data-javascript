d39407e79ab6c4a0db8d67aefe7f1570f00c3b3f
Lars Eckart <lars.eckart@hey.com>
2021-12-02 21:36:59 +0200

    document more examples for the OR combinator

--- test/validate_integers_test.js
+++ test/validate_integers_test.js
@@ -1,8 +1,8 @@
-import { test } from "zora";
+import {test} from "zora";
 
 function validate(num, validation, failureMessage) {
   let result = validation(num);
-  return result ? { result } : { result, error: failureMessage(num) };
+  return result ? {result} : {result, error: failureMessage(num)};
 }
 
 function validateEven(num) {
@@ -30,7 +30,7 @@ function validatePositive(num) {
 }
 
 test(`positive result?`, (assertions) => {
-  assertions.equal(validatePositive(37), { result: true });
+  assertions.equal(validatePositive(37), {result: true});
   assertions.equal(validatePositive(-1), {
     result: false,
     error: "-1 is not positive",
@@ -38,7 +38,7 @@ test(`positive result?`, (assertions) => {
 });
 
 test(`negative?`, (assertions) => {
-  assertions.equal(validateNegative(-1), { result: true });
+  assertions.equal(validateNegative(-1), {result: true});
   assertions.equal(validateNegative(1), {
     result: false,
     error: "1 is not negative",
@@ -52,7 +52,7 @@ function validateAnd(num, validateF, validateG) {
   let result = first.result && second.result;
   let errors = [first, second].map((each) => each.error).filter((each) => each);
 
-  return result ? { result } : { result, errors };
+  return result ? {result} : {result, errors};
 }
 
 test(`is even and negative?`, (assertions) => {
@@ -94,7 +94,7 @@ test(`is even or negative?`, (assertions) => {
       .map((each) => each.error)
       .filter((each) => each);
 
-    return result ? { result } : { result, errors };
+    return result ? {result} : {result, errors};
   }
 
   assertions.equal(validateOr(-2, validateEven, validateNegative), {
@@ -103,6 +103,15 @@ test(`is even or negative?`, (assertions) => {
 
   assertions.equal(validateOr(-1, validateEven, validateNegative), {
     result: true,
+  });
+
+  assertions.equal(validateOr(2, validateEven, validateNegative), {
+    result: true,
+  });
+
+  assertions.equal(validateOr(3, validateEven, validateNegative), {
+    "result": false,
+    "errors": ["3 is not even", "3 is not negative"]
   });
 });
 
